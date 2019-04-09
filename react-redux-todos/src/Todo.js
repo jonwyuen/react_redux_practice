70682b8f2eae9099a5629e5e1f5f1572bc73a0d8
Jonathan Yuen <jonwyuen@gmail.com>
2019-04-08 22:33:46 -0700

    adding edit todo logic

--- react-redux-todos/src/Todo.js
+++ react-redux-todos/src/Todo.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
+import EditForm from "./EditForm";
 
 class Todo extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      showEditForm: false
+    };
     this.handleDelete = this.handleDelete.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
   }
 
   handleDelete() {
     this.props.removeTodo(this.props.id);
+  }
+
+  handleEdit() {
+    this.setState({
+      showEditForm: !this.state.showEditForm
+    });
   }
 
   render() {
@@ -15,6 +26,7 @@ class Todo extends Component {
       <div>
         <li>
           {this.props.todo}
+          <button onClick={this.handleEdit}>Edit</button>
           <button
             style={{ color: "red", marginLeft: "10px" }}
             onClick={this.handleDelete}
@@ -22,6 +34,7 @@ class Todo extends Component {
             X
           </button>
         </li>
+        {this.state.showEditForm ? <EditForm todo={this.props.todo} /> : null}
       </div>
     );
   }
