994451739f78cb210acdb410c5cb98d3c861d2fe
Jonathan Yuen <jonwyuen@gmail.com>
2019-04-12 00:38:54 -0700

    refactoring app and todolist into stateless functional components

--- react-redux-todos/src/App.js
+++ react-redux-todos/src/App.js
@@ -1,24 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import TodoListContainer from "./TodoListContainer";
 import "./App.css";
 import { Route, Link, Redirect } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Todo App</h1>
-        <p>
-          <Link to="/todos">Todos</Link>
-        </p>
-        <p>
-          <Link to="/todos/new">Add a todo</Link>
-        </p>
-        <Route path="/todos" component={TodoListContainer} />
-        <Route exact path="/" render={() => <Redirect to="/todos" />} />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Todo App</h1>
+      <p>
+        <Link to="/todos">Todos</Link>
+      </p>
+      <p>
+        <Link to="/todos/new">Add a todo</Link>
+      </p>
+      <Route path="/todos" component={TodoListContainer} />
+      <Route exact path="/" render={() => <Redirect to="/todos" />} />
+    </div>
+  );
+};
 
 export default App;
--- react-redux-todos/src/TodoList.js
+++ react-redux-todos/src/TodoList.js
@@ -1,71 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import Todo from "./Todo";
-import TodoForm from "./TodoForm";
-import { connect } from "react-redux";
-import { addTodo, removeTodo, editTodo } from "./actionCreators";
-import { Route } from "react-router-dom";
 
-class TodoList extends Component {
-  constructor(props) {
-    super(props);
-    this.addTodo = this.addTodo.bind(this);
-    this.removeTodo = this.removeTodo.bind(this);
-    this.editTodo = this.editTodo.bind(this);
-  }
+const TodoList = ({ todos, removeTodo }) => {
+  const noTodosMessage = <p>No current todos...</p>;
 
-  removeTodo(id) {
-    this.props.removeTodo(id);
-  }
-
-  addTodo(todo) {
-    this.props.addTodo(todo);
-  }
-
-  editTodo(id, todo) {
-    this.props.editTodo(id, todo);
-  }
-
-  render() {
-    const todos = this.props.todos.map(todo => (
-      <Todo
-        todo={todo.todo}
-        id={todo.id}
-        key={todo.id}
-        removeTodo={this.removeTodo}
-        editTodo={this.editTodo}
-      />
-    ));
-    return (
-      <div>
-        <Route
-          exact
-          path="/todos"
-          render={() => (
-            <div>
-              <ul>{todos}</ul>
-            </div>
-          )}
+  const todosList = (
+    <div>
+      {todos.map(todo => (
+        <Todo
+          task={todo.task}
+          id={todo.id}
+          key={todo.id}
+          removeTodo={removeTodo}
         />
-        <Route
-          path="/todos/new"
-          component={props => <TodoForm {...props} addTodo={this.addTodo} />}
-        />
-        <Route
-          path="/todos/:id/edit"
-          component={props => <TodoForm {...props} editTodo={this.editTodo} />}
-        />
-      </div>
-    );
-  }
-}
+      ))}
+    </div>
+  );
 
-const mapStateToProps = state => {
-  return {
-    todos: state.todos
-  };
+  return <div>{todos.length === 0 ? noTodosMessage : todosList}</div>;
 };
 
-export default connect(
-  mapStateToProps,
-  { addTodo, removeTodo, editTodo }
-)(TodoList);
+export default TodoList;
