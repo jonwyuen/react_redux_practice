f38be33f7ed724fd44a699fdc4ab2c50016a4bb3
Jonathan Yuen <jonwyuen@gmail.com>
2019-04-12 00:58:12 -0700

    removing edittodoform file and adding todoform container

--- react-redux-todos/src/App.js
+++ react-redux-todos/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TodoListContainer from "./TodoListContainer";
+import TodoFormContainer from "./TodoFormContainer";
 import "./App.css";
 import { Route, Link, Redirect } from "react-router-dom";
 
@@ -14,6 +15,8 @@ const App = () => {
         <Link to="/todos/new">Add a todo</Link>
       </p>
       <Route path="/todos" component={TodoListContainer} />
+      <Route path="/todos/new" component={TodoFormContainer} />
+      <Route path="/todos/:id/edit" component={TodoFormContainer} />
       <Route exact path="/" render={() => <Redirect to="/todos" />} />
     </div>
   );
