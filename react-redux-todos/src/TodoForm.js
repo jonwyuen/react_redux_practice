360cf716708ef8698171f91370096559e2c9f626
Jonathan Yuen <jonwyuen@gmail.com>
2019-04-13 15:10:59 -0700

    removing unnecessary redirect

--- react-redux-todos/src/TodoForm.js
+++ react-redux-todos/src/TodoForm.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
 
 class TodoForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
       todo: props.todo ? props.todo.todo : "",
-      id: props.todo ? props.todo.id : null,
-      redirect: false
+      id: props.todo ? props.todo.id : null
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -23,28 +21,24 @@ class TodoForm extends Component {
     e.preventDefault();
     this.props.handleSubmit(this.state.id, this.state.todo);
     e.target.reset();
-    this.setState({
-      redirect: true
-    });
   }
 
   render() {
-    const form = this.state.redirect ? (
-      <Redirect to="/todos" />
-    ) : (
-      <form onSubmit={this.handleSubmit}>
-        <label>Todo</label>
-        <input
-          type="text"
-          name="todo"
-          id="todo"
-          value={this.state.todo}
-          onChange={this.handleChange}
-        />
-        <button>Save Todo</button>
-      </form>
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Todo</label>
+          <input
+            type="text"
+            name="todo"
+            id="todo"
+            value={this.state.todo}
+            onChange={this.handleChange}
+          />
+          <button>Save Todo</button>
+        </form>
+      </div>
     );
-    return <div>{form}</div>;
   }
 }
 
