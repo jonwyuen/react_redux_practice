bee609a8a757328546bbdfdfb2d31df434b08495
Jonathan Yuen <jonwyuen@gmail.com>
2019-04-15 23:55:24 -0700

    updating root reducer with new action creators

--- react-redux-todos/src/rootReducer.js
+++ react-redux-todos/src/rootReducer.js
@@ -1,25 +1,22 @@
-import { ADD_TODO, REMOVE_TODO, EDIT_TODO } from "./actionCreators";
+import { ADD_TODO, REMOVE_TODO, EDIT_TODO, GET_TODOS } from "./actionCreators";
 
 const INITIAL_STATE = {
-  todos: [],
-  id: 0
+  todos: []
 };
 
 const rootReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case GET_TODOS:
+      return { ...state, todos: action.data };
     case ADD_TODO:
-      let newState = { ...state };
-      newState.id++;
-      return {
-        ...newState,
-        todos: [...newState.todos, { todo: action.todo, id: newState.id }]
-      };
+      return { ...state, todos: [...state.todos, action.todo] };
     case REMOVE_TODO:
-      let todos = state.todos.filter(todo => todo.id !== action.id);
+      // with mongo, use _id instead of id
+      let todos = state.todos.filter(todo => todo._id !== action.id);
       return { ...state, todos };
     case EDIT_TODO:
       let updatedTodos = state.todos.map(todo => {
-        if (todo.id === action.payload.id) todo.todo = action.payload.todo;
+        if (todo._id === action.payload.id) todo.todo = action.payload.todo;
         return todo;
       });
       return { ...state, todos: updatedTodos };
