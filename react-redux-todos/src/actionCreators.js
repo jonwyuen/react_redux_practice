ede593895f020e69b6a3ddac3897c9536f6fbd17
Jonathan Yuen <jonwyuen@gmail.com>
2019-04-15 23:32:15 -0700

    incorporating thunks with action creators

--- react-redux-todos/src/actionCreators.js
+++ react-redux-todos/src/actionCreators.js
@@ -1,24 +1,82 @@
 export const ADD_TODO = "ADD_TODO";
 export const REMOVE_TODO = "REMOVE_TODO";
 export const EDIT_TODO = "EDIT_TODO";
+export const GET_TODOS = "GET_TODOS";
 
-export const addTodo = todo => {
+const handleTodos = data => {
+  return {
+    type: GET_TODOS,
+    data
+  };
+};
+
+const handleAdd = todo => {
   return {
     type: ADD_TODO,
     todo
   };
 };
 
-export const removeTodo = id => {
+const handleRemove = id => {
   return {
     type: REMOVE_TODO,
     id
   };
 };
 
-export const editTodo = (id, todo) => {
+const handleEdit = (id, todo) => {
   return {
     type: EDIT_TODO,
     payload: { id, todo }
+  };
+};
+
+export const getTodos = () => {
+  return dispatch => {
+    return fetch("http://localhost:3001/api/todos")
+      .then(res => res.json())
+      .then(data => dispatch(handleTodos(data)))
+      .catch(err => console.log("Something went wrong...", err));
+  };
+};
+
+export const addTodo = todo => {
+  return dispatch => {
+    return fetch("http://localhost:3001/api/todos", {
+      method: "POST",
+      headers: new Headers({
+        "Content-Type": "application/json"
+      }),
+      body: JSON.stringify({ todo })
+    })
+      .then(res => res.json())
+      .then(data => dispatch(handleAdd(data)))
+      .catch(err => console.log("Something went wrong...", err));
+  };
+};
+
+export const removeTodo = id => {
+  return dispatch => {
+    return fetch(`http://localhost:3001/api/todos/${id}`, {
+      method: "DELETE"
+    })
+      .then(res => res.json())
+      .then(data => dispatch(handleRemove(id)))
+      .catch(err => console.log("Something went wrong...", err));
+  };
+};
+
+export const editTodo = (id, todo) => {
+  return dispatch => {
+    return fetch(`http://localhost:3001/api/todos/${id}`, {
+      method: "PATCH",
+      headers: new Headers({
+        "Content-Type": "application/json"
+      }),
+      body: JSON.stringify({ todo })
+    })
+      .then(res => res.json())
+      .then(data => dispatch(handleEdit(id, todo)))
+      .catch(err => console.log("Something went wrong...", err));
   };
 };
