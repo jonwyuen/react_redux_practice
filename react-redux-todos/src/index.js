9315e8abd62119d67a859a60756b6d2d3166055e
Jonathan Yuen <jonwyuen@gmail.com>
2019-04-15 23:07:24 -0700

    using redux thunk

--- react-redux-todos/src/index.js
+++ react-redux-todos/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { BrowserRouter } from "react-router-dom";
 import rootReducer from "./rootReducer";
+import thunk from "redux-thunk";
+// redux thunk allows us to write action creators that return a function instead of an action
+// thunk delays dispatch so that we can wait until some async action has completed to dispatch an action
 
 const store = createStore(
   rootReducer,
+  compose(applyMiddleware(thunk)),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
