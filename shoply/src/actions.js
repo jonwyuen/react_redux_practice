bcfa4ef56e4cb1023b726e38d384df12a6d644d4
Jonathan Yuen <jonwyuen@gmail.com>
2019-04-23 00:38:02 -0700

    working on add item on root reducer

--- shoply/src/actions.js
+++ shoply/src/actions.js
@@ -1,4 +1,11 @@
-export const addToCart = item => {};
+import { ADD_TO_CART, REMOVE_FROM_CART, APPLY_DISCOUNT } from "./actionTypes";
+
+export const addToCart = item => {
+  return {
+    type: ADD_TO_CART,
+    item
+  };
+};
 
 export const removeFromCart = item => {};
 
--- shoply/src/rootReducer.js
+++ shoply/src/rootReducer.js
@@ -1,14 +1,50 @@
 import { ADD_TO_CART, REMOVE_FROM_CART, APPLY_DISCOUNT } from "./actionTypes";
+import { calcCartTotal } from "./helpers";
+import uuid from "uuid/v4";
 
 const INITIAL_STATE = {
   cartItems: [],
-  cartTotal: 0.0
+  cartTotal: 0.0,
+  discountApplied: false,
+  discountAmount: 0
 };
+
+/* cartItems: [{
+  name:
+  quantity:
+  id:
+  price:
+}]
+
+*/
 
 const rootReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case ADD_TO_CART:
-      return;
+      const foundIndex = state.cartItems.findIndex(
+        item => item.name === action.item.name
+      );
+      let newItem = null;
+      if (foundIndex === -1) {
+        newItem = {
+          name: action.item.name,
+          id: uuid(),
+          price: action.item.price,
+          quantity: 1
+        };
+      } else {
+        const foundItem = { ...state.cartItems[foundIndex] };
+        foundItem.quantity++;
+        state.cartItems[foundIndex] = foundItem;
+      }
+      const cartItems = newItem
+        ? [...state.cartItems, newItem]
+        : [...state.cartItems];
+      return {
+        ...state,
+        cartItems,
+        cartTotal: calcCartTotal(cartItems, state.discountAmount)
+      };
     case REMOVE_FROM_CART:
       return;
     case APPLY_DISCOUNT:
