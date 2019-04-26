0a2014ef7315fe06d32d377f1c8d9ce26a9f6354
Jonathan Yuen <jonwyuen@gmail.com>
2019-04-26 00:54:40 -0700

    starting cart and cart container

---
+++ shoply/src/components/Cart.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Cart extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  
+  render() {
+    return(
+
+    )
+  }
+}
+
+
+export default Cart;
---
+++ shoply/src/containers/CartContainer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { connect } from "react-redux";
+import Cart from "../components/Cart";
+import { applyDiscount } from "../actions";
+
+const CartContainer = props => {
+  return <Cart {...props} />;
+};
+
+const mapStateToProps = state => {
+  return {
+    cartItems: state.cartItems,
+    cartValue: state.cartValue,
+    discountApplied: state.discountApplied,
+    discountAmount: state.discountAmount
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { applyDiscount }
+)(CartContainer);
