9b6479c9f9c7411d165d90598106e44021355a50
Jonathan Yuen <jonwyuen@gmail.com>
2019-04-26 23:57:44 -0700

    working on cart logic

--- shoply/src/components/Cart.js
+++ shoply/src/components/Cart.js
@@ -1,18 +1,104 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import CartIconsContainer from "../containers/CartIconsContainer";
 
 class Cart extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      discount: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleDiscount = this.handleDiscount.bind(this);
+    this.renderTable = this.renderTable.bind(this);
   }
 
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
 
-  
+  handleDiscount(e) {
+    e.preventDefault();
+    this.props.applyDiscount(this.state.discount);
+    this.setState({
+      discount: ""
+    });
+  }
+
+  renderTable() {
+    const rows = this.props.cartItems.map(item => {
+      return (
+        <tr key={item.id}>
+          <td className="text-center align-middle">{item.name}</td>
+          <td className="text-center align-middle">{item.name}</td>
+          <td className="text-center align-middle">{item.name}</td>
+          <td>
+            <CartIconsContainer item={item} />
+          </td>
+        </tr>
+      );
+    });
+
+    return (
+      <table className="table table-bordered table-striped">
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+
   render() {
-    return(
-
-    )
+    const {
+      cartItems,
+      cartTotal,
+      discountApplied,
+      discountAmount
+    } = this.props;
+    return cartItems.length > 0 ? (
+      <div>
+        {this.renderTable()}
+        <h4>
+          Total: ${cartTotal}{" "}
+          {discountApplied ? (
+            <span className="text-success">
+              {(discountAmount * 100).toFixed(0)}% - You saved $
+              {(cartTotal * (discountAmount * 100)).toFixed(2)}
+            </span>
+          ) : null}{" "}
+        </h4>
+      </div>
+    ) : null;
   }
 }
 
+Cart.propTypes = {
+  cartItems: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      id: PropTypes.string,
+      price: PropTypes.number,
+      quantity: PropTypes.number
+    })
+  ).isRequired,
+  cartTotal: PropTypes.number.isRequired,
+  discountApplied: PropTypes.isBool.isRequired,
+  discountAmount: PropTypes.number.isRequired
+};
+
+Cart.defaultProps = {
+  cartItems: [],
+  cartTotal: 0.0,
+  discountApplied: false,
+  discountAmount: 0
+};
 
 export default Cart;
