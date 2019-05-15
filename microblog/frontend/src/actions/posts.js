f035f2fec38a2a255bff0277a322469411155f27
Jonathan Yuen <jonwyuen@gmail.com>
2019-05-14 23:48:45 -0700

    add and remove posts actions

--- microblog/frontend/src/actions/posts.js
+++ microblog/frontend/src/actions/posts.js
@@ -26,3 +26,35 @@ export const getPostFromAPI = id => {
     return dispatch(getPost(response.data));
   };
 };
+
+const addPost = post => {
+  return {
+    type: ADD_POST,
+    post
+  };
+};
+
+export const addPostToAPI = (title, body, description) => {
+  return async dispatch => {
+    const response = await axios.post(`${API_URL}`, {
+      title,
+      body,
+      description
+    });
+    return dispatch(addPost({ post: response.data }));
+  };
+};
+
+const removePost = id => {
+  return {
+    type: REMOVE_POST,
+    id
+  };
+};
+
+export const removePostFromAPI = id => {
+  return async dispatch => {
+    await axios.delete(`${API_URL}/${id}`);
+    return dispatch(removePost(id));
+  };
+};
