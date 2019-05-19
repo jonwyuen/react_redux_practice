be78bd7cb62125813fc089be129c56cb0ddfe597
Jonathan Yuen <jonwyuen@gmail.com>
2019-05-18 17:04:22 -0700

    comment form

---
+++ microblog/frontend/src/components/CommentForm.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+
+class CommentForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSumbit(e) {
+    e.preventDefault();
+    this.props.submitCommentForm(this.state.text);
+    this.setState({ text: "" });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="commentform-text"
+              name="text"
+              value={this.state.text}
+              size="50"
+              placeholder="New Comment"
+              onChange={this.handleChange}
+            />
+          </div>
+          <button className="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default CommentForm;
