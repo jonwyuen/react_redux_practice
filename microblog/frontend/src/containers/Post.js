4083b0bcd133db88d6aae75fd66bde56cf0d9082
Jonathan Yuen <jonwyuen@gmail.com>
2019-05-19 23:54:03 -0700

    working on functions for post container

--- microblog/frontend/src/containers/Post.js
+++ microblog/frontend/src/containers/Post.js
@@ -1,9 +1,64 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+  getPostFromAPI,
+  updatePostInAPI,
+  sendVoteToAPI,
+  sendCommentToAPI,
+  removeCommentFromAPI,
+  removePostFromAPI
+} from "../actions/posts";
+import PostForm from "../components/PostForm";
+import CommentList from "../components/CommentList";
 import PostDisplay from "../components/PostDisplay";
+import CommentForm from "../components/CommentForm";
 
 class Post extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isEditing: false
+    };
+    this.toggleEdit = this.toggleEdit.bind(this);
+    this.editPost = this.editPost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.vote = this.vote.bind(this);
+    this.addComment = this.addComment.bind(this);
+    this.removeComment = this.removeComment.bind(this);
+  }
+
+  async componentDidMount() {
+    if (!this.props.post) {
+      await this.props.getPostFromAPI(this.props.id);
+    }
+  }
+
+  toggleEdit() {
+    this.setState({
+      isEditing: !this.state.isEditing
+    });
+  }
+
+  editPost({ title, body, description }) {
+    this.props.updatePostInAPI(this.props.post.id, title, description, body);
+    this.toggleEdit();
+  }
+
+  deletePost() {
+    this.props.removePostFromAPI(this.props.post.id);
+    this.props.history.push("/");
+  }
+
+  vote(direction) {
+    this.props.sendVoteToAPI(this.props.post.id, direction);
+  }
+
+  addComment(comment) {
+    this.props.sendCommentToAPI(this.props.post.id, comment);
+  }
+
+  deleteComment(commentId) {
+    this.props.removeCommentFromAPI(this.props.post.id, commentId);
   }
 
   render() {
@@ -18,4 +73,22 @@ class Post extends Component {
   }
 }
 
-export default Post;
+const mapStateToProps = (state, ownProps) => {
+  let id = ownProps.match.params.postId;
+  return {
+    id,
+    post: state.posts[id]
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  {
+    getPostFromAPI,
+    updatePostInAPI,
+    sendVoteToAPI,
+    sendCommentToAPI,
+    removeCommentFromAPI,
+    removePostFromAPI
+  }
+)(Post);
