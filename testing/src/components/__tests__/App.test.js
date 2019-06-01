932ed807489c96cfe946bc79ceb81fb2ba35433f
Jonathan Yuen <jonwyuen@gmail.com>
2019-06-01 01:44:40 -0700

    using absolute path exports

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { shallow } from "enzyme";
-import App from "../App";
-import CommentBox from "../CommentBox";
+import App from "components/App";
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
 
 // shallow renders just the component and none of its children
 
@@ -9,4 +10,9 @@ it("shows a comment box", () => {
   const wrapped = shallow(<App />);
   // find returns array of all instances of found component
   expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it("shows a comment list", () => {
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
