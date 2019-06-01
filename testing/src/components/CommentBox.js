f8f0b0ce48f906527bd7bdd219d09ac7f6fc9d5a
Jonathan Yuen <jonwyuen@gmail.com>
2019-06-01 02:04:47 -0700

    jsx for comment box

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 
-const CommentBox = () => {
-  return <div>Comment Box</div>;
-};
+class CommentBox extends Component {
+  state = { comment: "" };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    // call action creator and save comment
+    this.setState({ [e.target.name]: "" });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add a Comment</h4>
+        <textarea
+          onChange={this.handleChange}
+          name="comment"
+          value={this.state.comment}
+        />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+    );
+  }
+}
 
 export default CommentBox;
--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -6,13 +6,17 @@ import CommentList from "components/CommentList";
 
 // shallow renders just the component and none of its children
 
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
+
 it("shows a comment box", () => {
-  const wrapped = shallow(<App />);
   // find returns array of all instances of found component
   expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it("shows a comment list", () => {
-  const wrapped = shallow(<App />);
   expect(wrapped.find(CommentList).length).toEqual(1);
 });
