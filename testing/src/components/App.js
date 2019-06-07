86f0473fc3c60853631eb6c7fdd22f16b49257e3
Jonathan Yuen <jonwyuen@gmail.com>
2019-06-07 00:31:24 -0700

    wiring up auth state and buttons

--- testing/src/components/App.js
+++ testing/src/components/App.js
@@ -1,17 +1,49 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { Component } from "react";
+import { Route, Switch, Link } from "react-router-dom";
+import { connect } from "react-redux";
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
+import { changeAuth } from "actions";
 
-const App = () => {
-  return (
-    <div>
-      <Switch>
-        <Route path="/" exact component={CommentList} />
-        <Route path="/post" component={CommentBox} />
-      </Switch>
-    </div>
-  );
-};
+class App extends Component {
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post</Link>
+        </li>
+        <li>{this.renderAuthButton()}</li>
+      </ul>
+    );
+  }
 
-export default App;
+  renderAuthButton() {
+    return this.props.auth ? (
+      <button onClick={() => this.props.changeAuth(false)}>Sign Out</button>
+    ) : (
+      <button onClick={() => this.props.changeAuth(true)}>Sign In</button>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderHeader()}
+        <Switch>
+          <Route path="/" exact component={CommentList} />
+          <Route path="/post" component={CommentBox} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({ auth: state.auth });
+
+export default connect(
+  mapStateToProps,
+  { changeAuth }
+)(App);
