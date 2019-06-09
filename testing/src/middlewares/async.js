1b58f1f942a6ca08557a181f99d116b571151e33
Jonathan Yuen <jonwyuen@gmail.com>
2019-06-09 00:02:36 -0700

    async middleware

--- testing/src/middlewares/async.js
+++ testing/src/middlewares/async.js
@@ -1,1 +1,13 @@
-export default ({ dispatch }) => next => action => {};
+export default ({ dispatch }) => next => action => {
+  // check to see if action has promise on payload property
+  // if no, send action on to next middleware
+  if (!action.payload || !action.payload.then) {
+    return next(action);
+  }
+  // if yes, wait for it to resolve then make new action and send it through all
+  // middlewares again, but with data as payload, not the promise
+  action.payload.then(response => {
+    const newAction = { ...action, payload: response };
+    dispatch(newAction);
+  });
+};
