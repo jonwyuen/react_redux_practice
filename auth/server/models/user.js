c7fbca43a74fb9f149bc147cf48c2e902fbefc73
Jonathan Yuen <jonwyuen@gmail.com>
2019-06-11 01:19:59 -0700

    jwt and local strategies

--- auth/server/models/user.js
+++ auth/server/models/user.js
@@ -10,8 +10,9 @@ const userSchema = new Schema({
 // hook that runs before saving model
 userSchema.pre("save", next => {
   const user = this;
-  // salt + plain pw = (salt + hashed pw)
-  // salt + submitted pw = hashed pw -> compare hashed pw in db
+  // salt + plain pw = (salt + hashed pw) saved in db
+  // compare pws by pulling salt from db to encrypt submitted pw
+  // which turns into a hashed pw, then compare hashed pw in db
   // gen a salt then run cb
   bcrypt.genSalt(10, (err, salt) => {
     if (err) return next(err);
@@ -24,6 +25,13 @@ userSchema.pre("save", next => {
     });
   });
 });
+
+userSchema.methods.comparePassword = (candidatePw, cb) => {
+  bcrypt.compare(candidatePw, this.password, (err, isMatch) => {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 
 const ModelClass = mongoose.model("user", userSchema);
 
---
+++ auth/server/services/passport.js
@@ -0,0 +1,48 @@
+const passport = require("passport");
+const User = require("../models/user");
+const config = require("../config");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passwort-jwt").ExtractJwt;
+const LocalStrategy = require("passport-local");
+
+// create local strategy
+// when user signs in, verify email and pw using local strategy -> give token
+const localOptions = { usernameField: "email" };
+const localLogin = new LocalStrategy(localOptions, (email, password, done) => {
+  // verify email and pw, call done w/ user
+  // if it is correct email and pw, otherwise call done w/ false
+  User.findOne({ email }, (err, user) => {
+    if (err) return done(err);
+    if (!user) return done(null, false);
+    // compare pws
+    user.comparePassword(passowrd, (err, isMatch) => {
+      if (err) return done(err);
+      if (!isMatch) return done(null, false);
+      return done(null, user);
+    });
+  });
+});
+
+// setup options for jwt strategy
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader("authorization"),
+  secretOrKey: config.secret
+};
+
+// when user makes auth'd req, verify token with jwt strat -> resource access
+const jwtLogin = new JwtStrategy(jwtOptions, (payload, done) => {
+  // see if user id in payload exists in db
+  // if it does, call done with that, otherwise call done w/o user obj
+  User.findById(payload.sub, (err, user) => {
+    if (err) return done(err, false);
+
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
+
+passport.use(jwtLogin);
+passport.use(localLogin);
