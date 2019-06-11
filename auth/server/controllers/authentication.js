d9625a868894cf4275319f055d24a9025d7f22a6
Jonathan Yuen <jonwyuen@gmail.com>
2019-06-10 23:23:55 -0700

    using bcrypt

--- auth/server/controllers/authentication.js
+++ auth/server/controllers/authentication.js
@@ -3,6 +3,13 @@ const User = require("../models/user");
 exports.signup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: "Must provide both email and password" });
+  }
+
   User.findOne({ email }, (err, existingUser) => {
     if (err) return next(err);
 
--- auth/server/models/user.js
+++ auth/server/models/user.js
@@ -1,9 +1,26 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt-nodejs");
 
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
   password: String
+});
+
+// hook that runs before saving model
+userSchema.pre("save", next => {
+  const user = this;
+  // gen a salt then run cb
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+    // hash pw using salt
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) return next(err);
+      // overwrite pw with encrypted pw
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 const ModelClass = mongoose.model("user", userSchema);
