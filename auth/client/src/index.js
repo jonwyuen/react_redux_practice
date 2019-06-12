035e02aff8a09b1c962f5547d4d9f70ed585bb9d
Jonathan Yuen <jonwyuen@gmail.com>
2019-06-12 01:49:12 -0700

    adding redux

--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -1,14 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
 import App from "./components/App";
 import Welcome from "./components/Welcome";
+import Signup from "./components/auth/Signup";
+
+const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App>
-      <Route path="/" exact component={Welcome} />
-    </App>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App>
+        <Route path="/" exact component={Welcome} />
+        <Route path="/signup" component={Signup} />
+      </App>
+    </BrowserRouter>
+  </Provider>,
   document.querySelector("#root")
 );
