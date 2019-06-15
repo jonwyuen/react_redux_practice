82ce5c4c7cd21fbc73fa73a5bd7fe1618a5ccfe5
Jonathan Yuen <jonwyuen@gmail.com>
2019-06-14 23:00:34 -0700

    showing auth errors

--- auth/client/src/actions/index.js
+++ auth/client/src/actions/index.js
@@ -1,9 +1,13 @@
-import { AUTH_USER } from "./types";
+import { AUTH_USER, AUTH_ERROR } from "./types";
 import axios from "axios";
 
 const API_URL = process.env.API_URL || "http://localhost:3001";
 
 export const signup = ({ email, password }) => async dispatch => {
-  const request = await axios.post(`${API_URL}/signup`, { email, password });
-  return dispatch({ type: AUTH_USER, payload: request.data });
+  try {
+    const response = await axios.post(`${API_URL}/signup`, { email, password });
+    return dispatch({ type: AUTH_USER, payload: response.data.token });
+  } catch (err) {
+    return dispatch({ type: AUTH_ERROR, payload: "Email in use" });
+  }
 };
