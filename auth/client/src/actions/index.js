7150388ab973e6fb1007ccb0d7c6e04abcef8478
Jonathan Yuen <jonwyuen@gmail.com>
2019-06-14 23:16:08 -0700

    feature component skeleton

--- auth/client/src/actions/index.js
+++ auth/client/src/actions/index.js
@@ -3,11 +3,12 @@ import axios from "axios";
 
 const API_URL = process.env.API_URL || "http://localhost:3001";
 
-export const signup = ({ email, password }) => async dispatch => {
+export const signup = ({ email, password }, callback) => async dispatch => {
   try {
     const response = await axios.post(`${API_URL}/signup`, { email, password });
-    return dispatch({ type: AUTH_USER, payload: response.data.token });
+    dispatch({ type: AUTH_USER, payload: response.data.token });
+    callback();
   } catch (err) {
-    return dispatch({ type: AUTH_ERROR, payload: "Email in use" });
+    dispatch({ type: AUTH_ERROR, payload: "Email in use" });
   }
 };
