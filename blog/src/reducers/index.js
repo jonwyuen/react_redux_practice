7e56ba9d4735a37fc4e59a10a493270724e0c4f7
Jonathan Yuen <jonwyuen@gmail.com>
2019-06-27 02:04:03 -0700

    postsReducer

--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
+import postsReducer from "./postsReducer";
 
 export default combineReducers({
-  placeholder: () => console.log("replace later")
+  posts: postsReducer
 });
