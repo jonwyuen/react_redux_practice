23f7b326018d5224f8803aa68efc5fa1f54c74fe
Jonathan Yuen <jonwyuen@gmail.com>
2019-06-28 03:08:00 -0700

    fetchUsersAndPosts action creator

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,10 @@
 import { FETCH_POSTS } from "./types";
+import { FETCH_USER } from "./types";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPostsAndUsers = () => async dispatch => {
+  await dispatch(fetchPosts());
+};
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
@@ -9,3 +14,23 @@ export const fetchPosts = () => async dispatch => {
     payload: response.data
   });
 };
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({
+    type: FETCH_USER,
+    payload: response.data
+  });
+};
+
+// memoize: can only call action creator one time with each unique user id, can only fetch each user one time inside app
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({
+//     type: FETCH_USER,
+//     payload: response.data
+//   });
+// });
