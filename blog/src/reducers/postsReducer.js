c073e1ea107158d2d96772a63b2d47370a8de52c
Jonathan Yuen <jonwyuen@gmail.com>
2019-06-28 01:12:55 -0700

    working on postlist

--- blog/src/reducers/postsReducer.js
+++ blog/src/reducers/postsReducer.js
@@ -1,5 +1,10 @@
-export default () => {
-  return {
-    posts: []
-  };
+import { FETCH_POSTS } from "../actions/types";
+
+export default (state = [], action) => {
+  switch (action.type) {
+    case FETCH_POSTS:
+      return action.payload;
+    default:
+      return state;
+  }
 };
