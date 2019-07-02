14146d6e3e165f05a5f59d94eebe9ce1dd9462e2
Jonathan Yuen <jonwyuen@gmail.com>
2019-07-01 23:51:26 -0700

    working on auth reducer

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 class GoogleAuth extends React.Component {
   state = { isSignedIn: null };
@@ -19,15 +21,16 @@ class GoogleAuth extends React.Component {
     });
   }
 
-  onAuthChange = () => {
-    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  onAuthChange = isSignedIn => {
+    if (isSignedIn) this.props.signIn();
+    else this.props.signOut();
   };
 
-  onSignIn = () => {
+  onSignInClick = () => {
     this.auth.signIn();
   };
 
-  onSignOut = () => {
+  onSignOutClick = () => {
     this.auth.signOut();
   };
 
@@ -35,14 +38,14 @@ class GoogleAuth extends React.Component {
     if (this.state.isSignedIn === null) return null;
     else if (this.state.isSignedIn) {
       return (
-        <button className="ui red google button" onClick={this.onSignOut}>
+        <button className="ui red google button" onClick={this.onSignOutClick}>
           <i className="google icon" />
           Sign Out
         </button>
       );
     } else {
       return (
-        <button className="ui green google button" onClick={this.onSignIn}>
+        <button className="ui green google button" onClick={this.onSignInClick}>
           <i className="google icon" />
           Sign In with Google
         </button>
@@ -55,4 +58,7 @@ class GoogleAuth extends React.Component {
   }
 }
 
-export default GoogleAuth;
+export default connect(
+  null,
+  actions
+)(GoogleAuth);
