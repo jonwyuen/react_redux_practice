a750dc66372ac71ff62e6b81e536a6479ae5914e
Jonathan Yuen <jonwyuen@gmail.com>
2019-07-04 15:33:28 -0700

    showing error messages to field inputs

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -2,14 +2,26 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput({ input, label, meta }) {
+  renderError({ error, touched }) {
+    if (error && touched) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => {
+    const fieldClassName = `field ${meta.error && meta.touched ? "error" : ""}`;
     return (
-      <div className="field">
+      <div className={fieldClassName}>
         <label htmlFor={label}>{label}</label>
-        <input {...input} />
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
       </div>
     );
-  }
+  };
 
   onSubmit = formValues => {};
 
@@ -17,7 +29,7 @@ class StreamCreate extends React.Component {
     return (
       <form
         onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form"
+        className="ui form error"
       >
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field
@@ -37,7 +49,7 @@ const validate = formValues => {
     errors.title = "You must enter a title";
   }
   if (!formValues.description) {
-    errors.title = "You must enter a description";
+    errors.description = "You must enter a description";
   }
   return errors;
 };
