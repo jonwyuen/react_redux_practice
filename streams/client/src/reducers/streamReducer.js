90fe0bf6ebb650146bd5dbd8e7acbe5b138296f4
Jonathan Yuen <jonwyuen@gmail.com>
2019-07-07 15:01:23 -0700

    stream reducer

---
+++ streams/client/src/reducers/streamReducer.js
@@ -0,0 +1,33 @@
+import {
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM
+} from "../actions/types";
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAMS: {
+      // from api => [ { id: 1, title: "stream" }, { id: 2, title: "stream2" } ]
+      // state => { 1: { id: 1, title: "stream" }, 2: { id: 2, title: "stream2" } }
+      const newState = action.payload.reduce((acc, cur) => {
+        acc[cur.id] = cur;
+        return acc;
+      }, {});
+      return { ...state, newState };
+    }
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_STREAM: {
+      const { [action.payload]: omit, ...rest } = state;
+      return rest;
+    }
+    default:
+      return state;
+  }
+};
