fba0467620e0da7cc0b76ae5a2673e92801d916a
Jonathan Yuen <jonwyuen@gmail.com>
2019-07-09 15:41:57 -0700

    refactoring streamshow and using switch for router

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -15,8 +15,8 @@ const App = () => {
         <div>
           <Header />
           <Route path="/" exact component={StreamList} />
-          <Route path="/streams/show" component={StreamShow} />
           <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/:id" component={StreamShow} />
           <Route path="/streams/edit/:id" component={StreamEdit} />
           <Route path="/streams/delete/:id" component={StreamDelete} />
         </div>
--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+class StreamShow extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    return <div>StreamShow</div>;
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.stream[ownProps.match.params.id] };
 };
 
-export default StreamShow;
+export default connect(
+  mapStateToProps,
+  { fetchStream }
+)(StreamShow);
