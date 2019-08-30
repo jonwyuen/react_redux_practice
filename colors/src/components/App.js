8f164c51d8a3ea71d1d0324e8859f6e5a1ed7c47
Jonathan Yuen <jonwyuen@gmail.com>
2019-08-29 23:59:42 -0700

    adding palette to app

--- colors/src/components/App.js
+++ colors/src/components/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Palette from "./Palette";
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header" />
+      <Palette />
     </div>
   );
 };
