433356108eb2472c4395c9741c31351669172ed4
Jonathan Yuen <jonwyuen@gmail.com>
2019-09-08 23:57:29 -0700

    select from material ui on navbar

--- colors/src/components/Navbar.js
+++ colors/src/components/Navbar.js
@@ -2,6 +2,8 @@ import React from "react";
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
 import Slider from "rc-slider";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const Navbar = ({ level, changeLevel }) => {
   return (
@@ -20,6 +22,13 @@ const Navbar = ({ level, changeLevel }) => {
             onAfterChange={changeLevel}
           />
         </div>
+      </div>
+      <div className="select-container">
+        <Select>
+          <MenuItem value="hex">HEX = #ffffff</MenuItem>
+          <MenuItem value="rgb">RGB = rgb(255,255,255)</MenuItem>
+          <MenuItem value="rgba">RGBA = rgba(255,255,255,1.0)</MenuItem>
+        </Select>
       </div>
     </header>
   );
