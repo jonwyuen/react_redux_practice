61175df9e3837689d7415c6ea788fe239c9a07f3
Jonathan Yuen <jonwyuen@gmail.com>
2019-09-10 23:59:18 -0700

    material ui snackbar

--- colors/src/components/Navbar.js
+++ colors/src/components/Navbar.js
@@ -4,14 +4,21 @@ import "./Navbar.css";
 import Slider from "rc-slider";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
 
 const Navbar = ({ level, changeLevel, changeFormat }) => {
   const [format, setFormat] = useState("hex");
+  const [open, setOpen] = useState(false);
 
-  const handleChange = e => {
+  const handleFormatChange = e => {
     setFormat(e.target.value);
+    setOpen(true);
     changeFormat(e.target.value);
   };
+
+  const closeSnackbar = () => setOpen(false);
 
   return (
     <header className="Navbar">
@@ -31,12 +38,32 @@ const Navbar = ({ level, changeLevel, changeFormat }) => {
         </div>
       </div>
       <div className="select-container">
-        <Select value={format} onChange={handleChange}>
+        <Select value={format} onChange={handleFormatChange}>
           <MenuItem value="hex">HEX = #ffffff</MenuItem>
           <MenuItem value="rgb">RGB = rgb(255,255,255)</MenuItem>
           <MenuItem value="rgba">RGBA = rgba(255,255,255,1.0)</MenuItem>
         </Select>
       </div>
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        open={open}
+        autoHideDuration={3000}
+        message={
+          <span id="message-id">Format Changed To {format.toUpperCase()}</span>
+        }
+        ContentProps={{ "aria-describedby": "message-id" }}
+        onClose={closeSnackbar}
+        action={[
+          <IconButton
+            onClick={closeSnackbar}
+            color="inherit"
+            key="close"
+            aria-label="close"
+          >
+            <CloseIcon />
+          </IconButton>
+        ]}
+      />
     </header>
   );
 };
