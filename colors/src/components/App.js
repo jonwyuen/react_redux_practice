65a9a502886f374666eaba0cc0e9dbaaa8db8808
Jonathan Yuen <jonwyuen@gmail.com>
2019-09-12 00:24:11 -0700

    palette route

--- colors/src/components/App.js
+++ colors/src/components/App.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "../seedColors";
 import { generatePalette } from "../colorHelpers";
 
 const App = () => {
+  const findPalette = id => seedColors.find(palette => palette.id === id);
+
   return (
-    <div className="App">
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
+    <Switch>
+      <Route exact path="/" render={() => <h1>Palette List</h1>} />
+      <Route
+        exact
+        path="/palette/:id"
+        render={routeProps => (
+          <Palette
+            palette={generatePalette(findPalette(routeProps.match.params.id))}
+          />
+        )}
+      />
+    </Switch>
   );
 };
 
