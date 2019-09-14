fc2b3e5db2cc5cd7a4c4733e1df5dccb523e09cb
Jonathan Yuen <jonwyuen@gmail.com>
2019-09-14 01:47:30 -0700

    mini palette with jss

--- colors/src/components/PaletteList.js
+++ colors/src/components/PaletteList.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import MiniPalette from "./MiniPalette";
 
 const PaletteList = ({ palettes }) => {
   return (
     <div>
+      <MiniPalette />
       <h1>React Colors</h1>
       {palettes.map(palette => (
         <p>
