5aa64c8ca079c186e35c23f9691619ebb0ebeeae
Jonathan Yuen <jonwyuen@gmail.com>
2019-09-29 23:53:32 -0700

    NewPaletteColorsContext

---
+++ colors/src/context/NewPaletteColorsContext.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const NewPaletteColorsContext = React.createContext([ {}, () => {} ]);
+
+const NewPaletteColorsProvider = props => {
+	const [ colors, setColors ] = useState([ { color: "blue", name: "blue" } ]);
+
+	return (
+		<NewPaletteColorsContext.Provider value={(colors, setColors)}>
+			{props.children}
+		</NewPaletteColorsContext.Provider>
+	);
+};
+
+export { NewPaletteColorsContext, NewPaletteColorsProvider };
