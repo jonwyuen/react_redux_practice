7044434bf386591cfc7bc7efedc987033755ed60
Jonathan Yuen <jonwyuen@gmail.com>
2019-10-16 23:58:51 -0700

    make ColorBox responsive

--- colors/src/styles/ColorBoxStyles.js
+++ colors/src/styles/ColorBoxStyles.js
@@ -16,11 +16,11 @@ export default makeStyles({
 		},
 		[sizes.down("lg")]: {
 			width: "25%",
-			height: props => (props.showingFullPalette ? "20%" : "50%")
+			height: props => (props.showingFullPalette ? "20%" : "33.3333%")
 		},
 		[sizes.down("md")]: {
 			width: "50%",
-			height: props => (props.showingFullPalette ? "20%" : "50%")
+			height: props => (props.showingFullPalette ? "10%" : "20%")
 		},
 		[sizes.down("xs")]: {
 			width: "100%",
@@ -121,7 +121,10 @@ export default makeStyles({
 			textAlign: "center",
 			textTransform: "uppercase",
 			marginBottom: "0px",
-			padding: "1rem"
+			padding: "1rem",
+			[sizes.down("xs")]: {
+				fontSize: "6rem"
+			}
 		},
 		"& p": {
 			fontSize: "2rem",
