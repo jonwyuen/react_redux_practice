1e48897ceb3fbcc00227a06e6d4aa2e3ca8658c7
Jonathan Yuen <jonwyuen@gmail.com>
2019-10-20 00:29:03 -0700

    animating route transitions

--- colors/src/components/App.js
+++ colors/src/components/App.js
@@ -1,41 +1,71 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import Palette from "./Palette";
 import PaletteList from "./PaletteList";
 import SingleColorPalette from "./SingleColorPalette";
 import NewPaletteForm from "./NewPaletteForm";
 import { PalettesProvider } from "../context/PalettesContext";
 import { NewPaletteColorsProvider } from "../context/NewPaletteColorsContext";
+import "../styles/App.css";
 
 const App = () => {
 	return (
-		<PalettesProvider>
-			<Switch>
-				<Route
-					exact
-					path="/palette/new"
-					render={routeProps => (
-						<NewPaletteColorsProvider>
-							<NewPaletteForm {...routeProps} />
-						</NewPaletteColorsProvider>
-					)}
-				/>
-				<Route
-					exact
-					path="/"
-					render={routeProps => <PaletteList {...routeProps} />}
-				/>
-				<Route
-					exact
-					path="/palette/:id"
-					render={routeProps => <Palette {...routeProps} />}
-				/>
-				<Route
-					path="/palette/:paletteId/:colorId"
-					render={routeProps => <SingleColorPalette {...routeProps} />}
-				/>
-			</Switch>
-		</PalettesProvider>
+		<Route
+			render={({ location }) => (
+				<TransitionGroup>
+					<CSSTransition key={location.key} classNames="fade" timeout={500}>
+						<Switch location={location}>
+							<Route
+								exact
+								path="/palette/new"
+								render={routeProps => (
+									<PalettesProvider>
+										<NewPaletteColorsProvider>
+											<div className="page">
+												<NewPaletteForm {...routeProps} />
+											</div>
+										</NewPaletteColorsProvider>
+									</PalettesProvider>
+								)}
+							/>
+							<Route
+								exact
+								path="/"
+								render={routeProps => (
+									<div className="page">
+										<PalettesProvider>
+											<PaletteList {...routeProps} />
+										</PalettesProvider>
+									</div>
+								)}
+							/>
+							<Route
+								exact
+								path="/palette/:id"
+								render={routeProps => (
+									<div className="page">
+										<PalettesProvider>
+											<Palette {...routeProps} />
+										</PalettesProvider>
+									</div>
+								)}
+							/>
+							<Route
+								path="/palette/:paletteId/:colorId"
+								render={routeProps => (
+									<div className="page">
+										<PalettesProvider>
+											<SingleColorPalette {...routeProps} />
+										</PalettesProvider>
+									</div>
+								)}
+							/>
+						</Switch>
+					</CSSTransition>
+				</TransitionGroup>
+			)}
+		/>
 	);
 };
 
