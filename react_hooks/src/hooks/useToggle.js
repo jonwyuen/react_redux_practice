ce733e7862f9a5d5c48dabb387232f70e018d532
Jonathan Yuen <jonwyuen@gmail.com>
2019-12-11 15:33:06 -0800

    fix useToggle

--- react_hooks/src/hooks/useToggle.js
+++ react_hooks/src/hooks/useToggle.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 const useToggle = (initialVal = false) => {
 	const [ value, setValue ] = useState(initialVal);
-	const toggle = () => setValue(!initialVal);
+	const toggle = () => setValue(!value);
 	return [ value, toggle ];
 };
 
