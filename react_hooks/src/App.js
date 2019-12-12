348742e3e6aa6fcbe91c8b641015c3b57735627e
Jonathan Yuen <jonwyuen@gmail.com>
2019-12-12 15:33:56 -0800

    useEffect hook example

--- react_hooks/src/App.js
+++ react_hooks/src/App.js
@@ -3,12 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Toggler from "./Toggler";
 import SimpleFormHook from "./SimpleFormHook";
+import SWMovies from "./SWMovies";
 
 function App() {
 	return (
 		<div className="App">
 			<Toggler />
 			<SimpleFormHook />
+			<SWMovies />
 		</div>
 	);
 }
