62f5d2ceb7cc88a8bb4b903de99670c21474eb12
Jonathan Yuen <jonwyuen@gmail.com>
2019-12-16 00:54:04 -0800

    removing and toggling todos

--- todo_hooks/src/TodoList.js
+++ todo_hooks/src/TodoList.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Todo from "./Todo";
 import Paper from "@material-ui/core/Paper";
 import Divider from "@material-ui/core/Divider";
 import List from "@material-ui/core/List";
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, removeTodo, toggleTodo }) => {
   return (
     <Paper>
       <List>
         {todos.map(todo => (
-          <>
-            <Todo key={todo.id} task={todo.task} completed={todo.completed} />
+          <Fragment key={todo.id}>
+            <Todo
+              key={todo.id}
+              id={todo.id}
+              task={todo.task}
+              completed={todo.completed}
+              removeTodo={removeTodo}
+              toggleTodo={toggleTodo}
+            />
             <Divider />
-          </>
+          </Fragment>
         ))}
       </List>
     </Paper>
