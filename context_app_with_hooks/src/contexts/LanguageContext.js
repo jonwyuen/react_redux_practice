57feba57b9d608b4a8206f3ae32bd168b97f8076
Jonathan Yuen <jonwyuen@gmail.com>
2019-12-31 00:37:57 -0800

    rewriting LangaugeContext with hooks

--- context_app_with_hooks/src/contexts/LanguageContext.js
+++ context_app_with_hooks/src/contexts/LanguageContext.js
@@ -1,23 +1,14 @@
-import React, { Component, createContext } from "react";
+import React, { useState, createContext } from "react";
 
 export const LanguageContext = createContext();
 
-export class LanguageProvider extends Component {
-  state = {
-    language: "french"
-  };
+export const LanguageProvider = props => {
+  const [language, setLanguage] = useState("french");
+  const changeLanguage = e => setLanguage(e.target.value);
 
-  changeLanguage = e => {
-    this.setState({ language: e.target.value });
-  };
-
-  render() {
-    return (
-      <LanguageContext.Provider
-        value={{ ...this.state, changeLanguage: this.changeLanguage }}
-      >
-        {this.props.children}
-      </LanguageContext.Provider>
-    );
-  }
-}
+  return (
+    <LanguageContext.Provider value={{ language, changeLanguage }}>
+      {props.children}
+    </LanguageContext.Provider>
+  );
+};
