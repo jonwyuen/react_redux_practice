f1666dc42a18170522ee73fb6a95b5b7c9d09f2e
Jonathan Yuen <jonwyuen@gmail.com>
2019-12-31 01:17:25 -0800

    useToggleState hook and using hooks in ThemeContext

--- context_app_with_hooks/src/contexts/ThemeContext.js
+++ context_app_with_hooks/src/contexts/ThemeContext.js
@@ -1,23 +1,14 @@
-import React, { Component, createContext } from "react";
+import React, { createContext } from "react";
+import useToggleState from "../hooks/useToggleState";
 
 export const ThemeContext = createContext();
 
-export class ThemeProvider extends Component {
-  state = {
-    isDarkMode: true
-  };
+export const ThemeProvider = props => {
+  const [isDarkMode, toggleTheme] = useToggleState(false);
 
-  toggleTheme = () => {
-    this.setState({ isDarkMode: !this.state.isDarkMode });
-  };
-
-  render() {
-    return (
-      <ThemeContext.Provider
-        value={{ ...this.state, toggleTheme: this.toggleTheme }}
-      >
-        {this.props.children}
-      </ThemeContext.Provider>
-    );
-  }
-}
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
+};
