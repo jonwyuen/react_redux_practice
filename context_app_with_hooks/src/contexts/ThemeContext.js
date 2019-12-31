d5a469f3e2c39a51c3493c297262ebe52a944551
Jonathan Yuen <jonwyuen@gmail.com>
2019-12-30 23:55:03 -0800

    useContext hook in Form

---
+++ context_app_with_hooks/src/contexts/ThemeContext.js
@@ -0,0 +1,23 @@
+import React, { Component, createContext } from "react";
+
+export const ThemeContext = createContext();
+
+export class ThemeProvider extends Component {
+  state = {
+    isDarkMode: true
+  };
+
+  toggleTheme = () => {
+    this.setState({ isDarkMode: !this.state.isDarkMode });
+  };
+
+  render() {
+    return (
+      <ThemeContext.Provider
+        value={{ ...this.state, toggleTheme: this.toggleTheme }}
+      >
+        {this.props.children}
+      </ThemeContext.Provider>
+    );
+  }
+}
